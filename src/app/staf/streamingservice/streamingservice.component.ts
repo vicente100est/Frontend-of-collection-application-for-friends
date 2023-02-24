2b669acfd0252e24b67f1820f36f78c8dec89f5b
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-02-24 16:02:07 -0600

    Add Get Streaming Service STAF

--- src/app/staf/streamingservice/streamingservice.component.ts
+++ src/app/staf/streamingservice/streamingservice.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApistreamingService } from 'src/app/services/apistreaming.service';
 
 @Component({
   selector: 'app-streamingservice',
   templateUrl: './streamingservice.component.html',
   styleUrls: ['./streamingservice.component.scss']
 })
-export class StreamingserviceComponent {
+export class StreamingserviceComponent implements OnInit {
+
+  public lstStreamings: any[] = [];
+
+  constructor(
+    private _apistreaming: ApistreamingService
+  ) { }
+
+  ngOnInit(): void {
+    this.getStreaming();
+  }
+
+  getStreaming() {
+    this._apistreaming.getStreamingService().subscribe(response => {
+      if (response.success === 1) {
+        this.lstStreamings = response.data;
+      }
+      else {
+        alert("Error: " + response.message);
+      }
+    })
+  }
 
 }
