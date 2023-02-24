5fb74d599f8d9d705a11c4b8dc547db63ac2c22d
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-02-24 16:19:14 -0600

    Add Get MontlyPayment Staff

--- src/app/staf/monthly/monthly.component.ts
+++ src/app/staf/monthly/monthly.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApistreamingService } from 'src/app/services/apistreaming.service';
 
 @Component({
   selector: 'app-monthly',
   templateUrl: './monthly.component.html',
   styleUrls: ['./monthly.component.scss']
 })
-export class MonthlyComponent {
+export class MonthlyComponent implements OnInit {
+
+  public lstMonthly: any[] = [];
+
+  constructor(
+    private _apistreaming: ApistreamingService
+  ) { }
+
+  ngOnInit(): void {
+    this.getMonthly();
+  }
+
+  getMonthly() {
+    this._apistreaming.getMonthlyPayment().subscribe(response => {
+      if (response.success === 1) {
+        this.lstMonthly = response.data;
+      }
+      else {
+        alert("Error: " + response.message);
+      }
+    })
+  }
 
 }
