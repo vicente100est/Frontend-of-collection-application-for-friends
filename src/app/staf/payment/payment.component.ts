a6469ac89fecddcd3104d863a472254a29a06760
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2023-02-28 11:54:12 -0600

    Add Get Payment

--- src/app/staf/payment/payment.component.ts
+++ src/app/staf/payment/payment.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApistreamingService } from '../../services/apistreaming.service';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
+
+  public lstStatus: any[] = [];
+  public lstPayments: any[] = [];
+
+  constructor(
+    private _apistreaming: ApistreamingService
+  ) {}
+
+  ngOnInit(): void {
+    this.getStatusPayment();
+    this.getPayments(1);
+  }
+
+  getPayments(idStatus: number) {
+    this._apistreaming.getPaymentByStatus(idStatus).subscribe(response => {
+      if (response.success === 1) {
+        this.lstPayments = response.data;
+      }
+      else {
+        alert("Error: " + response.message);
+      }
+    })
+  }
+
+  getStatusPayment() {
+    this._apistreaming.getStatus().subscribe(response => {
+      if (response.success === 1) {
+        this.lstStatus = response.data;
+      }
+      else {
+        alert("Error: " + response.message);
+      }
+    })
+  }
 
 }
